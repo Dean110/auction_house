0e2f7bb84d1a9236079b22be1ebdc1b786511596
Benjamin Williams <deanpbr@gmail.com>
2022-05-28 12:59:22 -0400

    Initial commit for auction_house
    Generated with `bundle gem auction_house --test=minitest`
    Added JetBrains product files to `.gitignore`

---
+++ lib/auction_house.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "auction_house/version"
+
+module AuctionHouse
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/auction_house/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module AuctionHouse
+  VERSION = "0.1.0"
+end
